819bcbe8b9f1312f8e840638417d83edc5d030dd
devyani-sil <devyanisil04@gmail.com>
2026-05-10 14:28:48 +0530

    rest api get and post calls

--- banking-app/src/main/java/com/example/banking_app/service/AccountService.java
+++ banking-app/src/main/java/com/example/banking_app/service/AccountService.java
@@ -5,4 +5,6 @@ import com.example.banking_app.dto.AccountDto;
 public interface AccountService {
 
     AccountDto createAccount(AccountDto account);
+
+    AccountDto getAccountById(Long id);
 }
--- banking-app/src/main/java/com/example/banking_app/service/impl/AccountServiceImpl.java
+++ banking-app/src/main/java/com/example/banking_app/service/impl/AccountServiceImpl.java
@@ -23,4 +23,11 @@ public class AccountServiceImpl implements AccountService {
         Account savedAccount = accountRepository.save(account);
         return AccountMapper.mapToAccountDto(savedAccount);
     }
+
+    @Override
+    public AccountDto getAccountById(Long id) {
+
+       Account account =  accountRepository.findById(id).orElseThrow(() -> new RuntimeException("Account does not exists"));
+        return AccountMapper.mapToAccountDto(account);
+    }
 }
