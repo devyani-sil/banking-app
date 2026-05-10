06da1a55fc5ecd5515ad6d3edf9503047d8366fd
devyani-sil <devyanisil04@gmail.com>
2026-05-10 15:06:46 +0530

    Deposit and withdraw Rest apis

--- banking-app/src/main/java/com/example/banking_app/controller/AccountController.java
+++ banking-app/src/main/java/com/example/banking_app/controller/AccountController.java
@@ -6,6 +6,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/accounts")
 public class AccountController {
@@ -26,6 +28,26 @@ public class AccountController {
     @GetMapping("/{id}")
     public ResponseEntity<AccountDto> getAccountById(@PathVariable Long id){
         AccountDto accountDto = accountService.getAccountById(id);
+        return ResponseEntity.ok(accountDto);
+    }
+
+    //Deposit Rest api
+    @PutMapping("/{id}/deposit")
+    public ResponseEntity<AccountDto> deposit(@PathVariable Long id, @RequestBody Map<String, Double> request){
+
+        Double amount = request.get("amount");
+
+        AccountDto accountDto = accountService.deposit(id, request.get("amount"));
+        return ResponseEntity.ok(accountDto);
+
+    }
+
+    //withdraw Rest Api
+    @PutMapping("/{id}/withdraw")
+    public ResponseEntity<AccountDto> withdraw(@PathVariable Long id,
+                                               @RequestBody Map<String, Double> request){
+        double amount = request.get("amount");
+        AccountDto accountDto = accountService.withdraw(id, amount);
         return ResponseEntity.ok(accountDto);
     }
 
